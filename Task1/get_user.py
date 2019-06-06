3c777a60b73afde9eaf813e9379a4dffa63983a2
nerianighthawk <hiroya.nakakawaji@gmail.com>
2019-06-06 21:57:51 +0900

    Task1 basic

--- Task1/get_user.py
+++ Task1/get_user.py
@@ -4,13 +4,7 @@ import sys
 try:
     input_id = int(input("ユーザIDを入力してください："))
     user_rep = user_repository.UserRepository()
-    key_user = user_rep.get_user_by_id(input_id)
-    user_rep.output(key_user)
+    user_rep.output_one_user(input_id)
 except ValueError:
     print("ユーザIDには整数を入力してください。")
     sys.exit()
-except IndexError:
-    print("指定されたIDのユーザは存在しません。")
-    sys.exit()
-except FileNotFoundError:
-    print("User.csvファイルが見つかりません。")
--- Task1/user_repository.py
+++ Task1/user_repository.py
@@ -1,5 +1,6 @@
 from Task1 import user_module
 from functools import reduce
+import sys
 
 
 class UserRepository:
@@ -7,13 +8,26 @@ class UserRepository:
     HEADER = "id,name,mail,phone_number\n"
 
     def __init__(self):
-        f = open("User.csv", "r", encoding="utf_8")
-        f.__next__()
-        lines = f.readlines()
-        f.close()
-        self.__users_list = list(map(self.string_to_object, lines))
+        try:
+            f = open("User.csv", "r", encoding="utf_8")
+            f.__next__()
+            lines = f.readlines()
+            f.close()
+            self.__users_list = list(map(self.string_to_object, lines))
+        except FileNotFoundError:
+            print("ファイルが見つかりませんでした。")
+            sys.exit()
+        except IOError:
+            print("ファイルの読み込みに失敗しました。")
+            sys.exit()
 
     def add_user(self, name: str, mail: str, phone_number: int):
+        """
+        :param name: 追加するユーザ名
+        :param mail: 追加するユーザのメールアドレス
+        :param phone_number: 追加するユーザの電話番号
+        :return: 戻り値なし
+        """
         new_id = self.get_new_id()
         user = user_module.User(new_id, name, mail, phone_number)
         self.__users_list.append(user)
@@ -27,60 +41,50 @@ class UserRepository:
         new_id = now_max_id + 1
         return new_id
 
-    @staticmethod
-    def get_user_by_id(key_id: int) -> user_module.User:
+    def get_user_by_id(self, key_id: int) -> user_module.User:
         """
         :param key_id: 取得したいUserインスタンスのID
         :return: 取得したUserインスタンス
         """
-        with open("User.csv", "r", encoding="utf_8") as f:
-            f.__next__()
-            lines = f.readlines()
-        user_string = list(filter(lambda x: int(x.split(",")[0]) == key_id, lines))
-        user_param_list = user_string[0].rstrip("\n").split(",")
-        user_id = int(user_param_list[0])
-        name = user_param_list[1]
-        mail = user_param_list[2]
-        phone_number = int(user_param_list[3])
-        user = user_module.User(user_id, name, mail, phone_number)
-        return user
+        try:
+            result = list(filter(lambda x: x.id_num == key_id, self.__users_list))
+            return result[0]
+        except IndexError:
+            print("そのIDのユーザは存在しません。")
+            sys.exit()
 
     def write_csv(self):
         """
         :return: 戻り値なし
         """
-        f = open("User.csv", "w", encoding="utf_8")
-        f.write(self.HEADER)
-        f.close()
-        map(self.write_user, self.__users_list)
+        try:
+            users_string_list = map(self.object_to_string, self.__users_list)
+            users_string = reduce(lambda x, y: x + y, users_string_list)
+            f = open("User.csv", "w", encoding="utf_8")
+            f.write(self.HEADER)
+            f.write(users_string)
+            f.close()
+        except IOError:
+            print("ファイルの書き込みに失敗しました。")
+            sys.exit()
 
-    @staticmethod
-    def output(user: user_module.User):
+    def output_one_user(self, key_id: int):
         """
-        :param user: コンソールに出力するユーザインスタンス
+        :param key_id: コンソールに出力するユーザインスタンス
         :return: 戻り値なし
         """
+        user = self.get_user_by_id(key_id)
         print("ID:{}".format(user.id_num))
         print("名前:{}".format(user.name))
         print("メールアドレス:{}".format(user.mail))
         print("電話番号:{}".format(user.phone_number))
 
     @staticmethod
-    def remove_user_from_csv(user: user_module.User):
-        """
-        :param user: 削除するユーザインスタンス
-        :return: 戻り値なし
-        """
-        with open("User.csv", "r", encoding="utf_8") as f:
-            header = f.readline()
-            lines = f.readlines()
-        users_string = [header] + list(filter(lambda x: int(x.split(",")[0]) != user.id_num, lines))
-        result_string = reduce(lambda x, y: x + y, users_string)
-        with open("User.csv", "w", encoding="utf_8"):
-            f.write(result_string)
-
-    @staticmethod
     def string_to_object(line: str) -> user_module.User:
+        """
+        :param line: カンマ区切りのユーザパラメータ文字列
+        :return: 読み込んだ結果のユーザインスタンス
+        """
         user_param_list = line.rstrip("\n").split(",")
         user_id = int(user_param_list[0])
         name = user_param_list[1]
@@ -90,7 +94,9 @@ class UserRepository:
         return user
 
     @staticmethod
-    def write_user(user: user_module.User):
-        f = open("User.csv", "a", encoding="utf_8")
-        f.write("{},{},{},{}\n".format(user.id_num, user.name, user.mail, user.phone_number))
-        f.close()
+    def object_to_string(user: user_module.User) -> str:
+        """
+        :param user: 文字列にしたいユーザインスタンス
+        :return: カンマ区切りのユーザパラメータ文字列
+        """
+        return "{},{},{},{}\n".format(user.id_num, user.name, user.mail, user.phone_number)
