0f7a921aab964d9599f5eb63b491cfb62719d089
nerianighthawk <hiroya.nakakawaji@gmail.com>
2019-06-06 17:58:33 +0900

    edit Task1

---
+++ Task1/add_user.py
@@ -0,0 +1,17 @@
+from Task1 import user_repository
+from Task1 import user_module
+import sys
+
+try:
+    name = input("名前を入力してください：")
+    mail = input("メールアドレスを入力してください：")
+    phone_number = int(input("電話番号を入力してください："))
+    user_rep = user_repository.UserRepository()
+    user_rep.add_user(name, mail, phone_number)
+    user_rep.write_csv()
+except ValueError:
+    print("電話番号には数値を入力してください。")
+    sys.exit()
+except FileNotFoundError:
+    print("User.csvファイルが見つかりません。")
+    sys.exit()
---
+++ Task1/get_user.py
@@ -0,0 +1,16 @@
+from Task1 import user_repository
+import sys
+
+try:
+    input_id = int(input("ユーザIDを入力してください："))
+    user_rep = user_repository.UserRepository()
+    key_user = user_rep.get_user_by_id(input_id)
+    user_rep.output(key_user)
+except ValueError:
+    print("ユーザIDには整数を入力してください。")
+    sys.exit()
+except IndexError:
+    print("指定されたIDのユーザは存在しません。")
+    sys.exit()
+except FileNotFoundError:
+    print("User.csvファイルが見つかりません。")
